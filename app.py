f2e439771034e5df2fe0c59381dbe2b18b3c5763
Silvio <silviolima@gmail.com>
2022-05-07 11:57:42 -0300

    teste

--- app.py
+++ app.py
@@ -77,13 +77,13 @@ def draw_app():
         )
 
         # Do something interesting with the image data and paths
-        if canvas_result.image_data is not None:
-            st.image(canvas_result.image_data)
-        if canvas_result.json_data is not None:
-            objects = pd.json_normalize(canvas_result.json_data["objects"])
-            for col in objects.select_dtypes(include=["object"]).columns:
-                objects[col] = objects[col].astype("str")
-            st.dataframe(objects)
+        #if canvas_result.image_data is not None:
+        #    st.image(canvas_result.image_data)
+        #if canvas_result.json_data is not None:
+        #    objects = pd.json_normalize(canvas_result.json_data["objects"])
+        #    for col in objects.select_dtypes(include=["object"]).columns:
+        #        objects[col] = objects[col].astype("str")
+        #    st.dataframe(objects)
 
 
 
