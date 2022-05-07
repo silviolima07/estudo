3df6cc052f29e55de6e3d5a17d811e86177ef6c3
Silvio <silviolima@gmail.com>
2022-05-07 20:21:45 -0300

    teste

--- app.py
+++ app.py
@@ -113,8 +113,8 @@ def png_export():
         st.session_state["button_id"] = re.sub(
             "\d+", "", str(uuid.uuid4()).replace("-", "")
         )
-    teste = os.rename('tmp/'+st.session_state["button_id"], 'numero_desenhado')
-    st.write(teste)
+    #teste = os.rename('tmp/'+st.session_state["button_id"], 'numero_desenhado')
+    #st.write(teste)
     button_id = st.session_state["button_id"]
     file_path = f"tmp/{button_id}.png"
 
