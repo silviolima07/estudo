0794f17e40d8cba75b962967e91b944584e572f4
silviolima07 <silviolima07@gmail.com>
2022-05-07 01:04:28 -0300

    teste

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
+from bokeh.plotting import figure
+from bokeh.models import FreehandDrawTool
+
 import streamlit as st
-
-from bokeh.models.widgets import Div
-
-import pandas as pd
-
-
-pd.set_option('precision',2)
-
-
-import sys
-
-import time
 
 
 
@@ -31,6 +22,16 @@ def main():
     </div>
               """
     st.markdown(html_page, unsafe_allow_html=True)
+    
+    p = figure(x_range=(0, 10), y_range=(0, 10), width=400, height=400)
+
+    renderer = p.multi_line([[1, 1]], [[1, 1]], line_width=1, alpha=0.4, color='red')
+
+    draw_tool = FreehandDrawTool(renderers=[renderer], num_objects=99999)
+    p.add_tools(draw_tool)
+    p.toolbar.active_drag = draw_tool
+
+    st.bokeh_chart(p)  
     
     
     
