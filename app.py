10c35f094582e97dacac1ac425b57800618cd1a0
silviolima07 <silviolima07@gmail.com>
2022-05-09 17:59:45 -0300

    teste

--- app.py
+++ app.py
@@ -185,7 +185,7 @@ def png_export():
         st.markdown(dl_link, unsafe_allow_html=True)
         
         convert_tensor = transforms.ToTensor()
-        file = "{file_path}"
+        file = file_path
         img = Image.open(file)
         file_tensor = convert_tensor(img)
         st.write("Imagem na forma de tensor")
