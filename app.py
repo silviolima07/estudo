06092f2b96dd4443a5d55d0ad4aa01986b61d522
silviolima07 <silviolima07@gmail.com>
2022-05-09 15:36:57 -0300

    tensor

--- app.py
+++ app.py
@@ -124,7 +124,8 @@ def png_export():
     button_id = st.session_state["button_id"]
     file_path = f"tmp/{button_id}.png"
 
-
+    st.subheader(file_path)
+    
     custom_css = f""" 
         <style>
             #{button_id} {{
