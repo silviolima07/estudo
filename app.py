fc3676710dd8a6d9b7ef6ed882d13d9feda0ab2e
silviolima07 <silviolima07@gmail.com>
2022-05-10 14:10:00 -0300

    teste

--- app.py
+++ app.py
@@ -224,7 +224,7 @@ def png_export():
             pred = modelo_keras.predict(img_784)
             st.title(pred.argmax())
             
-            pred2 = mnist_keras.predict(img_normalizado.reshape(1, 28, 28, 1))
+            pred2 = mnist_keras.predict(img_normalizado.reshape(-1, 28*28))
             st.subheader(pred2.argmax())
             
             
