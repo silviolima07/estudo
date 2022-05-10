f29f9ef89d802aaf70d66dc44d8460f1db90b3fa
silviolima07 <silviolima07@gmail.com>
2022-05-10 10:01:04 -0300

    teste

--- app.py
+++ app.py
@@ -200,7 +200,7 @@ def png_export():
         st.image(img_28_28)
         img_arr = np.array(img_28_28) / 255
         st.write(img_arr)
-        st.subheader(modelo_keras.predict_classes(img_prep,1,verbose=0)[0])
+        st.subheader(modelo_keras.predict_classes(img_arr,1,verbose=0)[0])
     
         
 
