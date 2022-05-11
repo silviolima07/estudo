bd0377ea932df6b1dd9a3b7d77025dc152c60f5c
silviolima07 <silviolima07@gmail.com>
2022-05-11 10:24:51 -0300

    teste

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def draw_app():
         stroke_color = st.sidebar.color_picker("Stroke color hex: ")
         bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
         bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
-        realtime_update = st.sidebar.checkbox("Update in realtime", True)
+        realtime_update = st.sidebar.checkbox("Update in realtime", False)
 
         # Create a canvas component
         canvas_result = st_canvas(
