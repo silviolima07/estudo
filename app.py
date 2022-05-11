913bc14231f304ac3c9d09116a941c6cb5a02f48
silviolima07 <silviolima07@gmail.com>
2022-05-11 10:42:57 -0300

    teste

--- app.py
+++ app.py
@@ -60,24 +60,25 @@ def draw_app():
     
     with st.echo("below"):
         # Specify canvas parameters in application
-        drawing_mode = st.sidebar.selectbox(
-            "Drawing tool:",
-            ("freedraw", "line", "rect", "circle", "transform", "polygon", "point"),
-        )
-        stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
-        if drawing_mode == 'point':
-            point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
-        stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-        bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
-        bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
-        realtime_update = st.sidebar.checkbox("Update in realtime", False)
+        #drawing_mode = st.sidebar.selectbox(
+        #    "Drawing tool:",
+        #    ("freedraw", "line", "rect", "circle", "transform", "polygon", "point"),
+        #)
+        #stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
+        stroke_width = 10
+        #if drawing_mode == 'point':
+        #    point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
+        #stroke_color = st.sidebar.color_picker("Stroke color hex: ")
+        #bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
+        #bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
+        #realtime_update = st.sidebar.checkbox("Update in realtime", False)
 
         # Create a canvas component
         canvas_result = st_canvas(
             fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
             stroke_width=stroke_width,
             stroke_color=stroke_color,
-            background_color=bg_color,
+            background_color="rgba(0, 0, 0, 0.3)",
             background_image=Image.open(bg_image) if bg_image else None,
             update_streamlit=realtime_update,
             height=150,
@@ -90,6 +91,13 @@ def draw_app():
         # Do something interesting with the image data and paths
         if canvas_result.image_data is not None:
             st.image(canvas_result.image_data)
+            
+            img_data = canvas_result.image_data
+            im = Image.fromarray(img_data.astype("uint8"), mode="RGB")
+            im.save(file_path, "PNG")
+            img_28_28 = img.resize([28,28], Image.Resampling.NEAREST)
+            st.image(img_28_28)
+            
         if canvas_result.json_data is not None:
             objects = pd.json_normalize(canvas_result.json_data["objects"])
             for col in objects.select_dtypes(include=["object"]).columns:
