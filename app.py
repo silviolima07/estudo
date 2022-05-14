d8cce0bdd105a410ba97ce743d4c8b29c9bf243b
silviolima07 <silviolima07@gmail.com>
2022-05-14 19:22:46 -0300

    teste

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def main():
     if "color_to_label" not in st.session_state:
         st.session_state["color_to_label"] = {}
     PAGES = {
-        "Basic example": full_app,
+        "Draw numbers": full_app,
     }
     page = st.sidebar.selectbox("Page:", options=list(PAGES.keys()))
     PAGES[page]()
@@ -37,11 +37,11 @@ def full_app():
         #    "Drawing tool:",
         #    ("freedraw")
         #)
-        stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
+        stroke_width = 10 #st.sidebar.slider("Stroke width: ", 1, 25, 3)
         if drawing_mode == 'point':
             point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
-        stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-        bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
+        stroke_color = "rgba(255, 255, 255)" #st.sidebar.color_picker("Stroke color hex: ")
+        bg_color = "rgba(0, 0, 0)" # st.sidebar.color_picker("Background color hex: ", "#eee")
         bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
         realtime_update = st.sidebar.checkbox("Update in realtime", True)
 
@@ -53,7 +53,7 @@ def full_app():
             background_color=bg_color,
             background_image=Image.open(bg_image) if bg_image else None,
             update_streamlit=realtime_update,
-            height=150,
+            height=450,
             drawing_mode=drawing_mode,
             point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
             display_toolbar=st.sidebar.checkbox("Display toolbar", True),
