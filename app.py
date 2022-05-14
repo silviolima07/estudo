4e08d6ff602c1a7dca0c293c0a23fbed123557b1
silviolima07 <silviolima07@gmail.com>
2022-05-14 17:03:21 -0300

    teste

--- app.py
+++ app.py
@@ -75,11 +75,10 @@ def full_app():
             # Create a canvas component
         canvas_result = st_canvas(
             fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-            stroke_width=stroke_width,
-            stroke_color=stroke_color,
-            background_color=bg_color,
-            background_image=Image.open(bg_image) if bg_image else None,
-            update_streamlit=realtime_update,
+            stroke_color="rgba(255, 255, 255)",
+            background_color="rgba(0, 0, 0)",
+            background_image=None,
+            update_streamlit=True,
             height=150,
             drawing_mode=drawing_mode,
             point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
