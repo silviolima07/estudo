8b1207cf49be563f74fddc63d00cbc583ae3b385
silviolima07 <silviolima07@gmail.com>
2022-05-14 17:31:38 -0300

    teste

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def full_app():
         st.image(img_28_28)
 
 
-"""
+
 def png_export():
     
     try:
@@ -112,30 +112,36 @@ def png_export():
     button_id = st.session_state["button_id"]
     file_path = f"tmp/{button_id}.png"
 
-    
+    custom_css = f""" 
+        <style>
+            #{button_id} {{
+                display: inline-flex;
+                align-items: center;
+                justify-content: center;
+                background-color: rgb(255, 255, 255);
+                color: rgb(38, 39, 48);
+                padding: .25rem .75rem;
+                position: relative;
+                text-decoration: none;
+                border-radius: 4px;
+                border-width: 1px;
+                border-style: solid;
+                border-color: rgb(230, 234, 241);
+                border-image: initial;
+            }} 
+            #{button_id}:hover {{
+                border-color: rgb(246, 51, 102);
+                color: rgb(246, 51, 102);
+            }}
+            #{button_id}:active {{
+                box-shadow: none;
+                background-color: rgb(246, 51, 102);
+                color: white;
+                }}
+        </style> """
 
-    data = st_canvas(update_streamlit=False, key="png_export")
-    if data is not None and data.image_data is not None:
-        img_data = data.image_data
-        im = Image.fromarray(img_data.astype("uint8"), mode="RGBA")
-        im.save(file_path, "PNG")
 
-        buffered = BytesIO()
-        im.save(buffered, format="PNG")
-        img_data = buffered.getvalue()
-        try:
-            # some strings <-> bytes conversions necessary here
-            b64 = base64.b64encode(img_data.encode()).decode()
-        except AttributeError:
-            b64 = base64.b64encode(img_data).decode()
 
-        dl_link = (
-            custom_css
-            + f'<a download="{file_path}" id="{button_id}" href="data:file/txt;base64,{b64}">Export PNG</a><br></br>'
-        )
-        st.markdown(dl_link, unsafe_allow_html=True)
-
-"""
 
 if __name__ == "__main__":
     st.set_page_config(
