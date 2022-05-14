64cc21a717adb09252c1d0de45abf6350c922583
silviolima07 <silviolima07@gmail.com>
2022-05-14 15:35:28 -0300

    teste

--- app.py
+++ app.py
@@ -60,31 +60,31 @@ def full_app():
     )
 
     with st.echo("below"):
-        # Specify canvas parameters in application
         drawing_mode = st.sidebar.selectbox(
             "Drawing tool:",
-            ("freedraw"),
+            ("freedraw", "line", "rect", "circle", "transform", "polygon", "point"),
         )
-        stroke_width = 10 #st.sidebar.slider("Stroke width: ", 1, 25, 3)
-       
-        #stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-        #bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
-        #bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
-        realtime_update = True # st.sidebar.checkbox("Update in realtime", True)
+        stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
+        if drawing_mode == 'point':
+            point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
+        stroke_color = st.sidebar.color_picker("Stroke color hex: ")
+        bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
+        bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
+        realtime_update = st.sidebar.checkbox("Update in realtime", True)
 
         # Create a canvas component
         canvas_result = st_canvas(
-            fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-            stroke_width=stroke_width,
-            stroke_color="rgba(255, 255, 255)",
-            background_color="rgba(0, 0, 0)",
-            background_image= None, # Image.open(bg_image) if bg_image else None,
-            update_streamlit=realtime_update,
-            height=150,
-            drawing_mode=drawing_mode,
-            point_display_radius= 0, #point_display_radius if drawing_mode == 'point' else 0,
-            display_toolbar= True, # st.sidebar.checkbox("Display toolbar", True),
-            key="full_app",
+        fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+        stroke_width=stroke_width,
+        stroke_color=stroke_color,
+        background_color=bg_color,
+        background_image=Image.open(bg_image) if bg_image else None,
+        update_streamlit=realtime_update,
+        height=150,
+        drawing_mode=drawing_mode,
+        point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
+        display_toolbar=st.sidebar.checkbox("Display toolbar", True),
+        key="full_app",
         )
 
         # Do something interesting with the image data and paths
