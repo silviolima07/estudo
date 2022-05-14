436fa9bc6acaa1f502bd74b27491a6e6be39aea2
silviolima07 <silviolima07@gmail.com>
2022-05-14 14:48:15 -0300

    teste

--- app.py
+++ app.py
@@ -21,26 +21,15 @@ def main():
     if "color_to_label" not in st.session_state:
         st.session_state["color_to_label"] = {}
     PAGES = {
-        "About": about,
+       # "About": about,
         "Basic example": full_app,
-        "Get center coords of circles": center_circle_app,
-        "Color-based image annotation": color_annotation_app,
-        "Download Base64 encoded PNG": png_export,
-        "Compute the length of drawn arcs": compute_arc_length,
+       # "Get center coords of circles": center_circle_app,
+       # "Color-based image annotation": color_annotation_app,
+       # "Download Base64 encoded PNG": png_export,
+       # "Compute the length of drawn arcs": compute_arc_length,
     }
     page = st.sidebar.selectbox("Page:", options=list(PAGES.keys()))
     PAGES[page]()
-
-    with st.sidebar:
-        st.markdown("---")
-        st.markdown(
-            '<h6>Made in &nbsp<img src="https://streamlit.io/images/brand/streamlit-mark-color.png" alt="Streamlit logo" height="16">&nbsp by <a href="https://twitter.com/andfanilo">@andfanilo</a></h6>',
-            unsafe_allow_html=True,
-        )
-        st.markdown(
-            '<div style="margin: 0.75em 0;"><a href="https://www.buymeacoffee.com/andfanilo" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="41" width="174"></a></div>',
-            unsafe_allow_html=True,
-        )
 
 
 def about():
@@ -75,11 +64,10 @@ def full_app():
         # Specify canvas parameters in application
         drawing_mode = st.sidebar.selectbox(
             "Drawing tool:",
-            ("freedraw", "line", "rect", "circle", "transform", "polygon", "point"),
+            ("Draw numbers from 0 to 9"),
         )
-        stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
-        if drawing_mode == 'point':
-            point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
+        stroke_width = 10 #st.sidebar.slider("Stroke width: ", 1, 25, 3)
+       
         stroke_color = st.sidebar.color_picker("Stroke color hex: ")
         bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
         bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
@@ -89,8 +77,8 @@ def full_app():
         canvas_result = st_canvas(
             fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
             stroke_width=stroke_width,
-            stroke_color=stroke_color,
-            background_color=bg_color,
+            stroke_color="rgba(255, 255, 255)",
+            background_color="rgba(0, 0, 0)",
             background_image=Image.open(bg_image) if bg_image else None,
             update_streamlit=realtime_update,
             height=150,
@@ -110,96 +98,7 @@ def full_app():
             st.dataframe(objects)
 
 
-def center_circle_app():
-    st.markdown(
-        """
-    Computation of center coordinates for circle drawings some understanding of Fabric.js coordinate system
-    and play with some trigonometry.
 
-    Coordinates are canvas-related to top-left of image, increasing x going down and y going right.
-
-    ```
-    center_x = left + radius * cos(angle * pi / 180)
-    center_y = top + radius * sin(angle * pi / 180)
-    ```
-    """
-    )
-    bg_image = Image.open("img/tennis-balls.jpg")
-
-    with open("saved_state.json", "r") as f:
-        saved_state = json.load(f)
-
-    canvas_result = st_canvas(
-        fill_color="rgba(255, 165, 0, 0.2)",  # Fixed fill color with some opacity
-        stroke_width=5,
-        stroke_color="black",
-        background_image=bg_image,
-        initial_drawing=saved_state
-        if st.sidebar.checkbox("Initialize with saved state", False)
-        else None,
-        height=400,
-        width=600,
-        drawing_mode="circle",
-        key="center_circle_app",
-    )
-    with st.echo("below"):
-        if canvas_result.json_data is not None:
-            df = pd.json_normalize(canvas_result.json_data["objects"])
-            if len(df) == 0:
-                return
-            df["center_x"] = df["left"] + df["radius"] * np.cos(
-                df["angle"] * np.pi / 180
-            )
-            df["center_y"] = df["top"] + df["radius"] * np.sin(
-                df["angle"] * np.pi / 180
-            )
-
-            st.subheader("List of circle drawings")
-            for _, row in df.iterrows():
-                st.markdown(
-                    f'Center coords: ({row["center_x"]:.2f}, {row["center_y"]:.2f}). Radius: {row["radius"]:.2f}'
-                )
-
-
-def color_annotation_app():
-    st.markdown(
-        """
-    Drawable Canvas doesn't provided out-of-the-box image annotation capabilities, but we can hack something with session state,
-    by mapping a drawing fill color to a label.
-
-    Annotate pedestrians, cars and traffic lights with this one, with any color/label you want 
-    (though in a real app you should rather provide your own label and fills :smile:).
-
-    If you really want advanced image annotation capabilities, you'd better check [Streamlit Label Studio](https://discuss.streamlit.io/t/new-component-streamlit-labelstudio-allows-you-to-embed-the-label-studio-annotation-frontend-into-your-application/9524)
-    """
-    )
-    with st.echo("below"):
-        bg_image = Image.open("img/annotation.jpeg")
-        label_color = (
-            st.sidebar.color_picker("Annotation color: ", "#EA1010") + "77"
-        )  # for alpha from 00 to FF
-        label = st.sidebar.text_input("Label", "Default")
-        mode = "transform" if st.sidebar.checkbox("Move ROIs", False) else "rect"
-
-        canvas_result = st_canvas(
-            fill_color=label_color,
-            stroke_width=3,
-            background_image=bg_image,
-            height=320,
-            width=512,
-            drawing_mode=mode,
-            key="color_annotation_app",
-        )
-        if canvas_result.json_data is not None:
-            df = pd.json_normalize(canvas_result.json_data["objects"])
-            if len(df) == 0:
-                return
-            st.session_state["color_to_label"][label_color] = label
-            df["label"] = df["fill"].map(st.session_state["color_to_label"])
-            st.dataframe(df[["top", "left", "width", "height", "fill", "label"]])
-
-        with st.expander("Color to label mapping"):
-            st.json(st.session_state["color_to_label"])
 
 
 def png_export():
@@ -280,36 +179,6 @@ def png_export():
         )
         st.markdown(dl_link, unsafe_allow_html=True)
 
-
-def compute_arc_length():
-    st.markdown(
-        """
-    Using an external SVG manipulation library like [svgpathtools](https://github.com/mathandy/svgpathtools)
-    You can do some interesting things on drawn paths.
-    In this example we compute the length of any drawn path.
-    """
-    )
-    with st.echo("below"):
-        bg_image = Image.open("img/annotation.jpeg")
-
-        canvas_result = st_canvas(
-            stroke_color="yellow",
-            stroke_width=3,
-            background_image=bg_image,
-            height=320,
-            width=512,
-            drawing_mode="freedraw",
-            key="compute_arc_length",
-        )
-        if (
-            canvas_result.json_data is not None
-            and len(canvas_result.json_data["objects"]) != 0
-        ):
-            df = pd.json_normalize(canvas_result.json_data["objects"])
-            paths = df["path"].tolist()
-            for ind, path in enumerate(paths):
-                path = parse_path(" ".join([str(e) for line in path for e in line]))
-                st.write(f"Path {ind} has length {path.length():.3f} pixels")
 
 
 if __name__ == "__main__":
