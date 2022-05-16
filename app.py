c819d6c39d44a90dd9e02e86fcd3ed54a685ef07
silviolima07 <silviolima07@gmail.com>
2022-05-15 23:41:37 -0300

    teste

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def full_app():
         
         img_pil = Image.fromarray(img)
         size_new = (28,28)
-        img_28_28 = img.resize(size_new, Image.LANCZOS)
+        img_28_28 = img_pil.resize(size_new, Image.LANCZOS)
         #img_28_28 = np.array(img_pil.resize((28, 28), Image.LANCZOS))
         
         
